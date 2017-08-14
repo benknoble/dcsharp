5b734305f96b47e16a60f622db82aacfafc77ba3
David Ben Knoble <ben.knoble@gmail.com>
2017-08-14 14:56:41 -0400

    Start Test suite

--- Test/DataElementTest.cs
+++ Test/DataElementTest.cs
@@ -1,2 +1,16 @@
-﻿// EmptyCSharpFile.cs
+﻿// DataElementTest.cs
 // 
+using NUnit.Framework;
+using DCSharp.Core;
+namespace Test
+{
+    [TestFixture]
+    public class DataElementTest
+    {
+        [Test]
+        public void ConstructDataElement ()
+        {
+            var d = new DataElement (0);
+        }
+    }
+}
--- Test/EngineTest.cs
+++ Test/EngineTest.cs
@@ -1,16 +1,26 @@
 ﻿// Test.cs
 // 
 using NUnit.Framework;
-using System;
+using DCSharp.Core;
 namespace Test
 {
     [TestFixture]
-    public class Test
+    public class EngineTest
     {
         [Test]
-        public void TestCase ()
+        public void ConstructEngine ()
         {
-            Assert.AreEqual (5, 5);
+            var e = new Engine ();
+        }
+
+        [Test]
+        public void Push ()
+        {
+            var e = new Engine ();
+            e.Push (new DataElement (0));
+            e.Push (new DataElement (decimal.MaxValue));
+            e.Push (new DataElement (decimal.MinValue));
+            e.Push (new DataElement (decimal.MinusOne));
         }
     }
 }
